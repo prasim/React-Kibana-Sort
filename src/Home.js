a00fd816c09f41d56eaeb28618d9c27d8c74ce30
prasim <prasim.jain@sap.com>
2020-12-30 09:05:47 +0530

    updates with kibana scp logs also sorted

--- src/Home.js
+++ src/Home.js
@@ -1,47 +1,110 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import { Button, InputGroup, FormControl, Accordion, Card } from 'react-bootstrap';
 class Home extends Component {
     constructor(props) {
       super(props);
       this.state = {
         logs: {},
-        value: ''
+        k8sValue: '',
+        scfValue: '',
       };
-      let localKibanaLogs = localStorage.getItem('kibanaLogs')
-      if (localKibanaLogs) {
+      let localk8sKibanaLogs = localStorage.getItem('k8sLogs')
+      let localscfKibanaLogs = localStorage.getItem('scfLogs')
+      if (localk8sKibanaLogs) {
         // localKibanaLogs = JSON.parse(localKibanaLogs)_
-        this.state.value = localKibanaLogs;
+        this.state.k8sValue = localk8sKibanaLogs;
       }
-      this.handleChange = this.handleChange.bind(this);
+      if (localscfKibanaLogs) {
+        // localKibanaLogs = JSON.parse(localKibanaLogs)_
+        this.state.scfValue = localscfKibanaLogs;
+      }
+      this.handlek8sChange = this.handlek8sChange.bind(this);
+      this.handlescfChange = this.handlescfChange.bind(this);
       this.submit = this.submit.bind(this);
     }
     
-    handleChange(event) {
-        this.setState({value: event.target.value});
+    handlek8sChange(event) {
+        this.setState({k8sValue: event.target.value});
+    }
+
+    handlescfChange(event) {
+      this.setState({scfValue: event.target.value});
+    }
+
+    getRandomColor() {
+      let color = "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+      return color;
     }
 
     submit(event) {
-        let jsonScript = JSON.parse(this.state.value)
-        localStorage.setItem('kibanaLogs', this.state.value);
-        let logsArray = jsonScript.responses[0].hits.hits
-        this.setState({logs: logsArray})
-        let logValue;
-        logsArray.forEach((logsEntry) => {
-            logValue = logsEntry._source.log;
-            let myRegexp = /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3,6})/;
-            let match = myRegexp.exec(logValue);
-            logsEntry.sortingKey = match[1]
+        localStorage.setItem('k8sLogs', this.state.k8sValue);
+        let k8slogsArray = [];
+        if (this.state.k8sValue) {
+          let k8sJsonScript = JSON.parse(this.state.k8sValue)
+          k8slogsArray = k8sJsonScript.responses[0].hits.hits
+        }
+        
+        localStorage.setItem('scfLogs', this.state.scfValue);
+        let scflogsArray = [];
+        if(this.state.scfValue) {
+          let scfJsonScript = JSON.parse(this.state.scfValue);
+          scflogsArray = scfJsonScript.responses[0].hits.hits
+        }
+                
+        scflogsArray.forEach((logsEntry, index) => {
+          logsEntry._source.log = logsEntry._source.msg;
+          let momentTime = moment.utc(logsEntry.sort[0], "x");
+          logsEntry._source.timeExact = momentTime.local().format('YYYY-MM-DD HH:mm:ss.SSS')
+          logsEntry.sort[1] = index;
         });
+        k8slogsArray.forEach((logsEntry, index) => {
+          let logValue = logsEntry._source.log;
+          let myRegexp = /(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3,6})/;
+          let match = myRegexp.exec(logValue);
+          let myRegexp2 = /(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3,6})/;
+          let match2 = myRegexp2.exec(logValue);
+          if (match && match[1]) {
+            let sortingKey = match[1];
+            let momentValue = moment.utc(sortingKey, "YYYY-MM-DD[T]h:mm:ss.SSSSSS");
+            let epoch = momentValue.valueOf();
+            logsEntry._source.timeExact = momentValue.local().format('YYYY-MM-DD HH:mm:ss.SSS');
+            logsEntry.sort[0] = epoch;
+            logsEntry.sort[1] = sortingKey;
+          } else if (match2 && match2[1]) {
+            let sortingKey = match2[1];
+            let momentValue = moment.utc(sortingKey, "YYYY-MM-DD h:mm:ss,SSS");
+            let epoch = momentValue.valueOf();
+            logsEntry._source.timeExact = momentValue.local().format('YYYY-MM-DD HH:mm:ss.SSS');
+            logsEntry.sort[0] = epoch;
+            logsEntry.sort[1] = index;
+          } else {
+            let momentTime = moment.utc(logsEntry.sort[0], "x");
+            logsEntry.doubtFull="[Doubtfull]";
+            logsEntry._source.timeExact = momentTime.local().format('YYYY-MM-DD HH:mm:ss.SSS')
+            logsEntry.sort[1] = index;
+          }
+          logsEntry._source.component_name = logsEntry._source.kubernetes.container_name;
+        });
+        let logsArray = [...k8slogsArray, ...scflogsArray];
         logsArray.sort((a, b) => {
-            if (a.sortingKey < b.sortingKey){
-                return 1;
+            if (a.sort[0] < b.sort[0]){
+              return 1;
             }
-            if (a.sortingKey > b.sortingKey) {
-                return -1;
+            if (a.sort[0] > b.sort[0]) {
+              return -1;
+            }
+            if (a.sort[1] < b.sort[1]){
+              return 1;
+            }
+            if (a.sort[1] > b.sort[1]) {
+              return -1;
             }
             return 0;
         })
-        console.log(jsonScript);
+        
+        this.setState({logs: logsArray})
+        console.log(logsArray);
     }
     
     render() {  
@@ -51,9 +114,11 @@ class Home extends Component {
                 <Accordion key={index}>
                 <Card>
                   <Card.Header>
-                    <Accordion.Toggle as={Button} variant="link" eventKey={''+ index}>
-                      Click me!
-                    </Accordion.Toggle>{entry._source.log}
+                    {entry.doubtFull}
+                    <span className="bubble red">{entry._source.component_name}</span>&nbsp;
+                    <span className="bubble green">{entry._source.timeExact}</span>&nbsp;
+                    <span className="bubble green">{entry.sort[0]}</span> &nbsp;
+                    <span className="bubble blue">{entry._source.log}</span> &nbsp;
                   </Card.Header>
                   <Accordion.Collapse eventKey={''+index}>
                     <Card.Body>{entry._source.log}</Card.Body>
@@ -66,9 +131,15 @@ class Home extends Component {
         <div className="width-80">
             <InputGroup className="mb-3">
                 <InputGroup.Prepend>
-                <InputGroup.Text>KIBANA Logs Object</InputGroup.Text>
+                <InputGroup.Text>KIBANA(K8S) Logs Object</InputGroup.Text>
                 </InputGroup.Prepend>
-                <FormControl as="textarea" aria-label="With textarea" value={this.state.value} onChange={this.handleChange}/>
+                <FormControl as="textarea" aria-label="With textarea" value={this.state.k8sValue} onChange={this.handlek8sChange}/>
+            </InputGroup>
+            <InputGroup className="mb-3">
+                <InputGroup.Prepend>
+                <InputGroup.Text>KIBANA(SCF) Logs Object</InputGroup.Text>
+                </InputGroup.Prepend>
+                <FormControl as="textarea" aria-label="With textarea" value={this.state.scfValue} onChange={this.handlescfChange}/>
             </InputGroup>
             <Button variant="primary" onClick={this.submit}>Submit</Button>
             {button} 
